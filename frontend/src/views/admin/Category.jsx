0a90909bd67a3762f5e87b35d809d886ec5bbb6c
22120278 <nvhphuc364@gmail.com>
2024-12-26 10:39:33 +0700

    remove unused search bar components from Category view

--- frontend/src/views/admin/Category.jsx
+++ frontend/src/views/admin/Category.jsx
@@ -26,23 +26,6 @@ const Category = () => {
       <div className="flex flex-wrap w-full">
         <div className="w-full lg:w-7/12">
           <div className="w-full p-4 bg-[#6a5fdf] rounded-md">
-            {/* // Searchbar  */}
-            <div className="flex justify-between items-center">
-              <select
-                onChange={(e) => setParPage(parseInt(e.target.value))}
-                className="px-4 py-2 outline-none bg-[#6a5fdf] border border-slate-700 rounded-md text-[#d0d2d6]"
-              >
-                <option value="5">5</option>
-                <option value="10">10</option>
-                <option value="20">20</option>
-              </select>
-              <input
-                className="px-4 py-2 outline-none bg-[#6a5fdf] border border-slate-700 rounded-md text-[#d0d2d6]"
-                type="text"
-                placeholder="Search"
-              />
-            </div>
-
             {/* // Category table  */}
             <div className="relative mt-5 overflow-x-auto">
               <table className="w-full text-sm text-left text-[#d0d2d6]">
