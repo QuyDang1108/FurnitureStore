14ace56dc2a4044eb74614d34031861acd3e44dd
22120278 <nvhphuc364@gmail.com>
2024-12-26 10:39:24 +0700

    add customer section to AdminDashboard and improve navigation links

--- frontend/src/views/admin/AdminDashboard.jsx
+++ frontend/src/views/admin/AdminDashboard.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { MdCurrencyYen, MdOutlineShoppingCart } from "react-icons/md";
 import { RiShoppingCartFill } from "react-icons/ri";
+import { FaUser } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 const AdminDashboard = () => {
@@ -53,99 +54,19 @@ const AdminDashboard = () => {
             <RiShoppingCartFill className="text-[white] shadow-lg" />
           </div>
         </div>
-      </div>
 
-      {/* // Messages */}
-      <div className="w-full flex mt-7 bg-[#6A5FDF] rounded-xl">
-        <div className="w-full p-4 text-[#D0D2D6]">
-          {/* // Header */}
-          <div className="flex justify-between items-center">
-            <h2 className="font-semibold text-lg text-[#D0D2D6] pb-3">
-              Recent seller messages
-            </h2>
-            <Link className="font-semibold text-sm text-[#D0D2D6] pb-3">
-              View all
-            </Link>
+        {/* // Customers */}
+        <div className="flex justify-between items-center p-5 bg-[#E6F9FF] rounded-md gap-3">
+          <div className="flex flex-col justify-start items-start text-[#5C5A5A]">
+            <h2 className="font-bold text-3xl">20</h2>
+            <span className="text-md font-medium">Customers</span>
           </div>
 
-          {/* // Unseen messages */}
-          <div className="flex flex-col gap-2 pt-6 text-[#D0D2D6]">
-            <ol className="relative border-1 border-slate-600 ml-4">
-              <li className="mb-3 ml-6 ">
-                <div className="flex absolute -left-5 shadow-lg justify-center items-center w-10 h-10 p-[6px] bg-[#4C7FE2] rounded-full z-10">
-                  <img
-                    className="w-full rounded-full h-full"
-                    src="http://localhost:3000/images/admin.jpg"
-                    alt="Admin avatar"
-                  />
-                </div>
-                <div className="p-3 bg-slate-800 rounded-lg border border-slate-600 shadow-sm">
-                  <div className="flex justify-between items-center mb-2">
-                    <Link className="text-md font-normal">Admin</Link>
-                    <time
-                      className="mb-1 text-sm font-normal sm:order-last sm:mb-0"
-                      datetime=""
-                    >
-                      2 days ago
-                    </time>
-                  </div>
-
-                  <div className="p-2 text-xs font-normal bg-slate-700 rounded-lg border border-slate-800">
-                    How are you
-                  </div>
-                </div>
-              </li>
-
-              <li className="mb-3 ml-6 ">
-                <div className="flex absolute -left-5 shadow-lg justify-center items-center w-10 h-10 p-[6px] bg-[#4C7FE2] rounded-full z-10">
-                  <img
-                    className="w-full rounded-full h-full"
-                    src="http://localhost:3000/images/admin.jpg"
-                    alt="Admin avatar"
-                  />
-                </div>
-                <div className="p-3 bg-slate-800 rounded-lg border border-slate-600 shadow-sm">
-                  <div className="flex justify-between items-center mb-2">
-                    <Link className="text-md font-normal">Admin</Link>
-                    <time
-                      className="mb-1 text-sm font-normal sm:order-last sm:mb-0"
-                      datetime=""
-                    >
-                      2 days ago
-                    </time>
-                  </div>
-
-                  <div className="p-2 text-xs font-normal bg-slate-700 rounded-lg border border-slate-800">
-                    How are you
-                  </div>
-                </div>
-              </li>
-
-              <li className="mb-3 ml-6 ">
-                <div className="flex absolute -left-5 shadow-lg justify-center items-center w-10 h-10 p-[6px] bg-[#4C7FE2] rounded-full z-10">
-                  <img
-                    className="w-full rounded-full h-full"
-                    src="http://localhost:3000/images/admin.jpg"
-                    alt="Admin avatar"
-                  />
-                </div>
-                <div className="p-3 bg-slate-800 rounded-lg border border-slate-600 shadow-sm">
-                  <div className="flex justify-between items-center mb-2">
-                    <Link className="text-md font-normal">Admin</Link>
-                    <time
-                      className="mb-1 text-sm font-normal sm:order-last sm:mb-0"
-                      datetime=""
-                    >
-                      2 days ago
-                    </time>
-                  </div>
-
-                  <div className="p-2 text-xs font-normal bg-slate-700 rounded-lg border border-slate-800">
-                    How are you
-                  </div>
-                </div>
-              </li>
-            </ol>
+          <div
+            className="w-[40px] h-[47px] rounded-full bg-[#0080FF] flex justify-center
+                    items-center text-xl"
+          >
+            <FaUser className="text-[white] shadow-lg" />
           </div>
         </div>
       </div>
@@ -157,7 +78,12 @@ const AdminDashboard = () => {
           <h2 className="font-semibold text-lg text-[#d0d2d6] pb-3 ">
             Recent Orders
           </h2>
-          <Link className="font-semibold text-sm text-[#d0d2d6]">View All</Link>
+          <Link
+            to={"/admin/orders"}
+            className="font-semibold text-sm text-[#d0d2d6]"
+          >
+            View All
+          </Link>
         </div>
 
         {/* // Table  */}
