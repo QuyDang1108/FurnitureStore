958a4202a546e971359edce87eed9b73cabe062b
22120278 <nvhphuc364@gmail.com>
2025-01-06 10:19:01 +0700

    feat: implement user authentication checks and enhance route management in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,34 @@
 import { useEffect, useState } from "react";
 import Router from "./router/Router";
 import publicRoutes from "./router/routes/publicRoutes";
-import { getRoute } from './router/routes/index';
+import { getRoute } from "./router/routes/index";
+import { get_user_info, getRole, log_out } from "./store/Reducers/authReducer";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
 
 function App() {
-  
-  const [allRoutes, setAllRoutes] = useState([...publicRoutes])
+  const [allRoutes, setAllRoutes] = useState([...publicRoutes]);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    const route = getRoute()
-    setAllRoutes([...allRoutes, route])
-  }, [])
+    const route = getRoute();
+    setAllRoutes([...allRoutes, route]);
+  }, []);
 
-  return <Router allRoutes={allRoutes} />
+  useEffect(() => {
+    const token = localStorage.getItem("accessToken");
+    if (token) {
+      const role = getRole(token);
+      if (!role) {
+        dispatch(log_out({ navigate }));
+      } else {
+        dispatch(get_user_info());
+      }
+    }
+  }, [dispatch, navigate]);
+
+  return <Router allRoutes={allRoutes} />;
 }
 
 export default App;
